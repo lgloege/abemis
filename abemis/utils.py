b33026c7c7559d31b635f555f46f2a64e7ae61aa
Luke Gloege <lukegloege@gmail.com>
2024-08-01 20:19:04 -0400

    fix: add wraps to decorator to preserve docstring

--- abemis/utils.py
+++ abemis/utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 
+from functools import wraps
+
 def convert_to_numpy(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         new_args = [np.array(arg) if not isinstance(arg, np.ndarray) else arg for arg in args]
         new_kwargs = {k: np.array(v) if not isinstance(v, np.ndarray) else v for k, v in kwargs.items()}

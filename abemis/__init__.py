1a26077dfb86c0c1299cb8858a5e9bed36f6282a
Luke Gloege <lukegloege@gmail.com>
2024-08-22 16:43:23 -0400

    feat: add stationary energy

--- abemis/__init__.py
+++ abemis/__init__.py
@@ -1,5 +1,6 @@
 """Set up module access for the base package."""
 
+from . import stationary_energy
 from . import ippu
 from . import transportation
 from . import waste
@@ -10,6 +11,7 @@ from . import efdb
 from .activity_based import general_formula
 
 __all__ = [
+    "stationary_energy",
     "efdb",
     "ippu",
     "transportation",
